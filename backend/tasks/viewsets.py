023ac0076a8fdf00abcbe1579f753a6e4030d83a
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2025-09-08 07:00:52 +0300

    Viewsets

--- backend/tasks/viewsets.py
+++ backend/tasks/viewsets.py
@@ -7,7 +7,12 @@ from tasks.models import Task
 class TaskViewSet(viewsets.ModelViewSet):
    serializer_class = TaskSerializer
    permission_classes = [IsAuthenticated]
-
+   filter_backends = [DjangoFilterBacked, filters.SearchFilter, filters.OrderingFilter]
+   filterset_fields = ['completed', 'due_date', 'status', 'priority']
+   search_fields = ['title', 'description']
+   ordering_fields = ['due_date', 'priority', 'created_at']
+   ordering = ['due_date']
+   
    def get_queryset(self):
        return Task.objects.filter(user=self.request.user)
 
